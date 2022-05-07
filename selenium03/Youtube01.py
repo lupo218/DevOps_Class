c0800f3a346381492c29e6e1db8c752eaae93272
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-05-07 08:54:57 +0300

    test8

--- selenium03/Youtube01.py
+++ selenium03/Youtube01.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-# Guy 4
+# Guy 5
 
 def Youtube():
     driver = webdriver.Chrome(executable_path='G:\chromedriver_win32\chromedriver.exe')
